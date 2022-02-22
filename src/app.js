727ca1e5e0f6ad1f135b97d3503af521e192125c
Nnebuife <95724536+Nnebuife@users.noreply.github.com>
2022-02-22 02:17:15 +0100

    weather icon updated

--- src/app.js
+++ src/app.js
@@ -12,18 +12,23 @@ let currentDate = document.querySelector("#current-day");
 currentDate.innerHTML =`${day}, ${hours} : ${minutes}`;
 
 
+
 let apiKey = "302c93e3e9e39b90c8c02a8d733c82ec"
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Brussels&appid=${apiKey}&units=metric`
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Montreal&appid=${apiKey}&units=metric`
 
 function getWeather(response) {
     let tempElement = document.querySelector("#main-temp");
     let descriptionElement = document.querySelector("#description");
     let windElement = document.querySelector("#speed-wind");
     let cityElement = document.querySelector("#city")
+    let iconELement = document.querySelector("#icon")
     tempElement.innerHTML = Math.round(response.data.main.temp);
     descriptionElement.innerHTML = response.data.weather[0].description;
     windElement.innerHTML = Math.round(response.data.wind.speed);
     cityElement.innerHTML = response.data.name;
+    iconELement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    iconELement.setAttribute("alt", response.data.weather[0].description);
+    
     
 }
 
