0e0cf2a8a0ef02370dfad55a2a2bf2b5850a56ba
Nnebuife <95724536+Nnebuife@users.noreply.github.com>
2022-02-22 01:34:31 +0100

    API integration updated

--- src/app.js
+++ src/app.js
@@ -11,5 +11,27 @@ if (minutes<10) {minutes = `0${minutes}`;}
 let currentDate = document.querySelector("#current-day");
 currentDate.innerHTML =`${day}, ${hours} : ${minutes}`;
 
-//let apiKey = "302c93e3e9e39b90c8c02a8d733c82ec"
-//let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}`
+
+let apiKey = "302c93e3e9e39b90c8c02a8d733c82ec"
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Brussels&appid=${apiKey}&units=metric`
+
+function getWeather(response) {
+    let tempElement = document.querySelector("#main-temp");
+    let descriptionElement = document.querySelector("#description");
+    let windElement = document.querySelector("#speed-wind");
+    let cityElement = document.querySelector("#city")
+    tempElement.innerHTML = Math.round(response.data.main.temp);
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+    cityElement.innerHTML = response.data.name;
+    
+}
+
+axios.get(apiUrl).then(getWeather);
+
+
+    
+
+
+
+
