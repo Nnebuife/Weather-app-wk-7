c3151466dde2e776ca7c3483514d81947b8a95d3
Nnebuife <95724536+Nnebuife@users.noreply.github.com>
2022-03-02 15:09:36 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -9,30 +9,21 @@ if (minutes<10) {minutes = `0${minutes}`;}
 let currentDate = document.querySelector("#current-day");
 currentDate.innerHTML =`${day}, ${hours} : ${minutes}`;
 
-function formatDay(timeStamp) {
-    let date = new Date(timeStamp * 1000);
-    let day = date.getDay();
-    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-    return days[day];    
-}
-
-function forecastWeather(response) {
-    let forecast = response.data.daily;
+function forecastWeather() {
     let displayForecast = document.querySelector("#forecast");
     let forecastHTML = `<div class ="row">`;
-    forecast.forEach(function (forecastDay, index) {
-        if (index < 6) {  forecastHTML += `<div class="col-2">
-        <div class="day-temp">${formatDay(forecastDay.weather[0].icon)} </div>
-            <img src= `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`  alt=" " width="36"/>
+    let days = ["Thu", "Fri", "Sat", "Sun","Mon"];
+    days.forEach(function (day) {
+        forecastHTML += `<div class="col-2">
+        <div class="day-temp">${day} </div>
+            <img src="https://openweathermap.org/img/wn/04n@2x.png" alt=" " width="36"/>
             <div class="temp-forecast">
                 <span class="temp-forecast-max">18°</span>
             <span class="temp-forecast-min">11°</span>
             </div>
             
-        </div>`        
-        }
-        ;
+        </div>`;
                 
     });
 
@@ -40,6 +31,7 @@ function forecastWeather(response) {
     displayForecast.innerHTML = forecastHTML;
     
 }
+
 function getForecast(coordinates) {
     let apiKey = "302c93e3e9e39b90c8c02a8d733c82ec";
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}`;
